ada4e9eff267c5a888fa647c7086d52f584ce981
schelpstech <sunkanmi@schelps.com.ng>
2021-10-19 07:22:29 +0100

    commit

---
+++ admin/admin_login.php
@@ -0,0 +1,83 @@
+<?php
+include "../backend/dbconfig.php";
+?>
+<!doctype html>
+<html class="no-js " lang="en">
+
+
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+<meta name="description" content="Responsive Bootstrap 4 and web Application ui kit.">
+
+<title>:: Owutech Admission Advisor :: Sign In</title>
+<!-- Favicon-->
+<link rel="icon" href="assets/images/logo.png" type="image/x-icon">
+<!-- Custom Css -->
+<link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/css/style.min.css">    
+</head>
+
+<body class="theme-blush">
+
+<div class="authentication">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-sm-12">
+                <form method="POST" action="../backend/dologin.php" class="card auth_form">
+                    <div class="header">
+                        <img class="logo" src="assets/images/logo.png" alt="">
+                        <h5> Admission Advisor Log in</h5>
+                        <?php
+							
+                            if (isset($_SESSION['reginfo']) && $_SESSION['reginfo'])
+                            {
+                              printf('<b>%s</b>', $_SESSION['reginfo']);
+                              unset($_SESSION['reginfo']);
+                            }
+                          ?>
+                    </div>
+                    <div class="body">
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Username" required="yes" name="uniqueid">
+                            <div class="input-group-append">
+                                <span class="input-group-text"><i class="zmdi zmdi-account-circle"></i></span>
+                            </div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="password" class="form-control" placeholder="Password" required="yes" autocomplete="new-password" name="uniquekey">
+                            <div class="input-group-append">                                
+                                <span class="input-group-text"><a href="forgot-password.html" class="forgot" title="Forgot Password"><i class="zmdi zmdi-lock"></i></a></span>
+                            </div>                            
+                        </div>
+                        <div class="checkbox">
+                            <input id="remember_me" type="checkbox" required="yes">
+                            <label for="remember_me">Remember Me</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-block waves-effect waves-light">SIGN IN</button>                        
+                       
+                    </div>
+                </form>
+                <div class="copyright text-center">
+                    &copy;
+                    <script>document.write(new Date().getFullYear())</script>,
+                    <span>Developed for Owutech by <a href="https://schelps.com.ng"> SCHELPS</a></span>
+                </div>
+            </div>
+            <div class="col-lg-8 col-sm-12">
+                <div class="card">
+                    <img src="assets/images/reg.jpg" alt="Sign In"/>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Jquery Core Js -->
+<script src="assets/bundles/libscripts.bundle.js"></script>
+<script src="assets/bundles/vendorscripts.bundle.js"></script>
+</body>
+
+
+</html>
